cc94960ee10bf3ed9c84b7b2117759694333e111
Luay Younus <luayghazwan@yahoo.com>
2017-08-12 23:47:03 -0700

    HackerRank - Easy - LinkedLists - Print in Reverse

---
+++ src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/LinkedLists/PrintInReverse.java
@@ -0,0 +1,26 @@
+package com.luay.DataStructuresAlgorithms.HackerRank.DataStructures.LinkedLists;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class PrintInReverse {
+    class Node {
+        int data;
+        Node next;
+    }
+    void ReversePrint(Node head) {
+        Stack<Integer> reversedLL = new Stack<Integer>();
+
+        if (head != null) {
+            Node current = head;
+            while(current != null){
+                reversedLL.push(current.data);
+                current = current.next;
+            }
+            while(!reversedLL.isEmpty()){
+                System.out.println((reversedLL.pop()));
+            }
+        }
+    }
+
+}
