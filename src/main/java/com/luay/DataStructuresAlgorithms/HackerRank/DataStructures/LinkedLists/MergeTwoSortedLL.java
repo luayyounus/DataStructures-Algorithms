527fed292cdc169f9d684c9fdf1c3cd99597ccda
Luay Younus <luayghazwan@yahoo.com>
2017-08-13 23:44:38 -0700

    HackerRank - LL - Easy - Merge Two Sorted Lists

---
+++ src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/LinkedLists/MergeTwoSortedLL.java
@@ -0,0 +1,14 @@
+package com.luay.DataStructuresAlgorithms.HackerRank.DataStructures.LinkedLists;
+
+public class MergeTwoSortedLL {
+    Node mergeLists(Node headA, Node headB) {
+        if (headA == null) return headB;
+        if (headB == null) return headA;
+        if(headA.data < headB.data){
+            headA.next = mergeLists(headA.next, headB);
+            return headA;
+        }
+        headB.next = mergeLists(headA,headB.next);
+        return headB;
+    }
+}
