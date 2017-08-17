12c0220486b7ed527c17812381f9d3609ca8a66e
Luay Younus <luayghazwan@yahoo.com>
2017-08-17 16:46:01 -0700

    HackerRank - Find Merge Point of Two Lists - Java

---
+++ src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/LinkedLists/FindMergePointOfTwoLists.java
@@ -0,0 +1,27 @@
+package com.luay.DataStructuresAlgorithms.HackerRank.DataStructures.LinkedLists;
+
+public class FindMergePointOfTwoLists {
+    int FindMergeNode(Node headA, Node headB) {
+        LinkedListLength length = new LinkedListLength();
+        int a = length.linkedListLength(headA);
+        int b = length.linkedListLength(headB);
+        int difference = a - b;
+        if(a < b){
+            Node temp = headA;
+            headA = headB;
+            headB = temp;
+            difference = b - a;
+        }
+        for(int i = 0; i<difference; i++){
+            headA = headA.next;
+        }
+        while(headA != null){
+            if(headA.data == headB.data){
+                return headA.data;
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return 0;
+    }
+}
