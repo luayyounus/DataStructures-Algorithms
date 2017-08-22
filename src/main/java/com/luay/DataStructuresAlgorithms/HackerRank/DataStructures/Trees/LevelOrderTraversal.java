f75d9f86f26d7fc70b29780314158d7c53a75c4e
Luay Younus <luayghazwan@yahoo.com>
2017-08-21 17:52:35 -0700

    Trees - Level Order Traversal - Java

---
+++ src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/Trees/LevelOrderTraversal.java
@@ -0,0 +1,17 @@
+package com.luay.DataStructuresAlgorithms.HackerRank.DataStructures.Trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    void levelOrder(Node root) {
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        while(!q.isEmpty()){
+            Node temp = q.poll();
+            System.out.print(temp.data + " ");
+            if(temp.left != null) q.add(temp.left);
+            if(temp.right != null) q.add(temp.right);
+        }
+    }
+}
