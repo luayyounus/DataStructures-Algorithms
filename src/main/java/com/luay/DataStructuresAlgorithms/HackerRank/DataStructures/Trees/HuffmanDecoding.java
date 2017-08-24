66233c7ac374a64e08ca0cebfe0ea50c90cb4276
Luay Younus <luayghazwan@yahoo.com>
2017-08-24 16:00:37 -0700

    Huffman Decoding Solution updated - Java

--- src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/Trees/HuffmanDecoding.java
+++ src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/Trees/HuffmanDecoding.java
@@ -1,4 +1,19 @@
 package com.luay.DataStructuresAlgorithms.HackerRank.DataStructures.Trees;
 
 public class HuffmanDecoding {
+    //Non-recursive
+    public void decode(String S ,NodeFrequency root){
+        NodeFrequency current = root;
+        for(int i = 0; i < S.length(); i++){
+            if(S.charAt(i) == '0'){
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+            if(current.data != '\0'){
+                System.out.print(current.data);
+                current = root;
+            }
+        }
+    }
 }
