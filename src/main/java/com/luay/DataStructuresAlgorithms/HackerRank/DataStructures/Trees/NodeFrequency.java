7c485704ea1523a4985ac38b3b80b94396abbe20
Luay Younus <luayghazwan@yahoo.com>
2017-08-24 16:01:39 -0700

    Node class with Frequency

---
+++ src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/Trees/NodeFrequency.java
@@ -0,0 +1,7 @@
+package com.luay.DataStructuresAlgorithms.HackerRank.DataStructures.Trees;
+
+public class NodeFrequency {
+    public  int frequency; // the frequency of this tree
+    public  char data;
+    public  NodeFrequency left, right;
+}
