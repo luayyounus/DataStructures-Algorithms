b72ce74a984edf3c61f94424f36aff8cde72e908
Luay Younus <luayghazwan@yahoo.com>
2017-08-28 23:07:41 -0700

    BST - Check BST or not. Another Solution

--- src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/Trees/CheckBST.java
+++ src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/Trees/CheckBST.java
@@ -4,13 +4,24 @@ public class CheckBST {
     boolean checkBST(Node root) {
         int min = 0;
         int max = 10000;
-        return checkBSTrecursion(root, min, max);
+        return checkBST(root, min, max);
     }
-    boolean checkBSTrecursion(Node root, int min, int max) {
+    boolean checkBST(Node root, int min, int max) {
         if(root == null) return true;
         if(root.data <= min || root.data >= max) return false;
-        if (!checkBSTrecursion(root.left, min, root.data)) return false;
-        if (!checkBSTrecursion(root.right, root.data, max)) return false;
+        if (!checkBST(root.left, min, root.data)) return false;
+        if (!checkBST(root.right, root.data, max)) return false;
         return true;
     }
+
+    // Another Solution
+    public int max = -1;
+    public boolean checkBST2(Node root) {
+        if(root == null) return true;
+        boolean left = checkBST2(root.left) && max < root.data;
+        max = root.data;
+        boolean right = checkBST2(root.right);
+        return left && right;
+    }
+
 }
