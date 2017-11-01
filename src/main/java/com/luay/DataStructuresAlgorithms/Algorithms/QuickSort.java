967bc27766949bfd1fed1d6c7c9de9ba9fb2f04b
Luay Younus <luayghazwan@yahoo.com>
2017-10-31 22:35:41 -0700

    Algorithms - Quick Sort

---
+++ src/main/java/com/luay/DataStructuresAlgorithms/Algorithms/QuickSort.java
@@ -0,0 +1,39 @@
+package com.luay.DataStructuresAlgorithms.Algorithms;
+
+// Time Complexity - O(n log n)
+// Space Complexity - O(n^2)
+
+public class QuickSort {
+    public static void quickSort(int[] array){
+        quickSort(array, 0, array.length - 1);
+    }
+
+    public static void quickSort(int[] array, int left, int right){
+        if (left >= right) return;
+        int pivot = array[(left + right) / 2];
+        int index = partition(array, left, right, pivot);
+        quickSort(array, left, index - 1);
+        quickSort(array, index, right);
+
+    }
+
+    public static int partition(int[] array, int left, int right, int pivot){
+        int temp;
+        while (left <= right) {
+            while (array[left] < pivot) {
+                left++;
+            }
+            while (array[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+}
