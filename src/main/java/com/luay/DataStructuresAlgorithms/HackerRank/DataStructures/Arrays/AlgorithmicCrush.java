51cf87772e3f3acd17daa06b213f640d1de017ed
Luay Younus <luayghazwan@yahoo.com>
2017-10-31 22:36:24 -0700

    HackerRank Algorithmic Crush

--- src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/Arrays/AlgorithmicCrush.java
+++ src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/Arrays/AlgorithmicCrush.java
@@ -1,29 +1,27 @@
 package com.luay.DataStructuresAlgorithms.HackerRank.DataStructures.Arrays;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class AlgorithmicCrush {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+        int n = 5;
+        int m = 2;
 
         long[] myList = new long[n];
-        for(int i=0;i<n;i++) myList[i]=0;
+        Arrays.fill(myList, 0);
 
-        int a;
-        int b;
-        long k;
-        for (int j=0; j<m ; j++){
-            a = scanner.nextInt();
-            b = scanner.nextInt();
-            k = scanner.nextInt();
-            myList[a-1] += k;
-            if(b < n) myList[b] -= k;
-        }
+        int a = 2;
+        int b = 4;
+        long k = 100;
+        myList[a-1] += k;
+        if(b < n) myList[b] -= k;
+
+        int x = 3;
+        int y = 5;
+        long z = 100;
+        myList[x-1] += z;
+        if(y < n) myList[y] -= z;
 
         long max = 0;
         long temp = 0;
