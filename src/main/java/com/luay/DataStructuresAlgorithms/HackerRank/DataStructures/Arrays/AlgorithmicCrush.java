7965c54813a1d9fbcfb4035e52c0963811f0179e
Luay Younus <luayghazwan@yahoo.com>
2017-10-31 22:40:18 -0700

    HackerRank Algorithmic Crush

--- src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/Arrays/AlgorithmicCrush.java
+++ src/main/java/com/luay/DataStructuresAlgorithms/HackerRank/DataStructures/Arrays/AlgorithmicCrush.java
@@ -5,23 +5,23 @@ import java.util.*;
 public class AlgorithmicCrush {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = 5;
-        int m = 2;
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
 
         long[] myList = new long[n];
-        Arrays.fill(myList, 0);
 
-        int a = 2;
-        int b = 4;
-        long k = 100;
-        myList[a-1] += k;
-        if(b < n) myList[b] -= k;
+        for(int i=0;i<n;i++) myList[i]=0;
 
-        int x = 3;
-        int y = 5;
-        long z = 100;
-        myList[x-1] += z;
-        if(y < n) myList[y] -= z;
+        int a;
+        int b;
+        long k;
+        for (int j=0; j<m ; j++){
+            a = scanner.nextInt();
+            b = scanner.nextInt();
+            k = scanner.nextInt();
+            myList[a-1] += k;
+            if(b < n) myList[b] -= k;
+        }
 
         long max = 0;
         long temp = 0;
