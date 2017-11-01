19c0921fa951b12611cdd2ac5218736598b45082
Luay Younus <luayghazwan@yahoo.com>
2017-10-31 22:34:35 -0700

    Algorithms - Bubble Sort

---
+++ src/main/java/com/luay/DataStructuresAlgorithms/Algorithms/BubbleSort.java
@@ -0,0 +1,32 @@
+package com.luay.DataStructuresAlgorithms.Algorithms;
+
+import java.util.Arrays;
+
+// Time Complexity - O(n^2)
+// Space Complexity - O(1)
+
+public class BubbleSort {
+    public static void bubbleSort(int[] array){
+        boolean isSorted = false;
+        int lastUnsorted = array.length - 1;
+        while (!isSorted){
+            int temp;
+            isSorted = true;
+            for (int i = 0; i < lastUnsorted; i++){
+                if(array[i] > array[i + 1]){
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    isSorted = false;
+                }
+            }
+            lastUnsorted--;
+        }
+    }
+    public static void main(String[] args){
+        int[] test = new int[]{1,7,9,3,5,6,4,8};
+        bubbleSort(test);
+        System.out.print(Arrays.toString(test));
+        int a = 0;
+    }
+}
