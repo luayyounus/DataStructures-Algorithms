134dc1d333115df9e4fac09b690de7829a5d2976
Luay Younus <luayghazwan@yahoo.com>
2017-11-02 22:00:47 -0700

    Leet Code - Move Zeros

---
+++ src/main/java/com/luay/DataStructuresAlgorithms/LeetCode/MoveZeros.java
@@ -0,0 +1,16 @@
+package com.luay.DataStructuresAlgorithms.LeetCode;
+
+public class MoveZeros {
+    //Time O(n)
+    //space O(1)
+    public void moveZeroes(int[] nums) {
+        int temp;
+        for(int i = 0, j = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                if(i != j) nums[i] = 0;
+                j++;
+            }
+        }
+    }
+}
