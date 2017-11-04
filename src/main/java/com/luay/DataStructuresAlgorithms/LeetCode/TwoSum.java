35dcddc8fe6737e9fac083d2a42cef8553739940
Luay Younus <luayghazwan@yahoo.com>
2017-11-03 23:56:09 -0700

    LeetCode - Two Sum

---
+++ src/main/java/com/luay/DataStructuresAlgorithms/LeetCode/TwoSum.java
@@ -0,0 +1,22 @@
+package com.luay.DataStructuresAlgorithms.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//https://leetcode.com/problems/two-sum/description/
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+}
